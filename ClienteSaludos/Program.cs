047fba60ed6649f6de17747b8427bfea2e16a222
Jose Ignacio Rodríguez García <JoseIgnacio.Rodriguez@tajamar365.com>
2016-02-18 11:05:14 +0100

    no message

---
+++ ClienteSaludos/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClienteSaludos.ServiciosSaludos;
+
+namespace ClienteSaludos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s=new SaludosServiceClient();
+            var saludo=new Saludo() {Idioma = "tr",Nombre = "Nacho"};
+
+            try
+            {
+                var r = s.Saludar(saludo);
+                Console.WriteLine(r);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
